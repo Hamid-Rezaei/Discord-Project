89c4ca3e24fb984328227200abaade68c49b814f
Hamid Rezaei <hamidrezaei.ce@gmail.com>
2022-07-10 05:17:48 +0430

    added in guild view

---
+++ Application/src/main/java/com/discordapp/View/InGuildViewController.java
@@ -0,0 +1,15 @@
+package com.discordapp.View;
+
+import com.discordapp.Model.Guild;
+import javafx.fxml.FXML;
+
+public class InGuildViewController {
+
+    public static Guild guild;
+
+    @FXML
+    public void initialize(){
+
+    }
+
+}
