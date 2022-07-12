adec6481c6b99f7a403d3d4f74bb5d45d5367e10
Hamid Rezaei <hamidrezaei.ce@gmail.com>
2022-07-12 18:32:08 +0430

    added block user scene

---
+++ Application/src/main/java/com/discordapp/View/BlockUserViewController.java
@@ -0,0 +1,18 @@
+package com.discordapp.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class BlockUserViewController {
+
+    @FXML
+    void BlockFriend(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cancel(MouseEvent event) {
+
+    }
+
+}
